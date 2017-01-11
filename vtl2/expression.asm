1d26a69cb398f14cc92c2f15c3c1fe092c8d1318
Paul Robson <paul@robsons.org.uk>
2017-01-11 09:53:33 +0000

    working except for > = <

--- vtl2/expression.asm
+++ vtl2/expression.asm
@@ -1,9 +1,47 @@
+; ***************************************************************************************************************
+; ***************************************************************************************************************
+;
+;		File:		expression.asm
+;		Purpose:	Evaluate an ASCII expression.
+;		Author:		Paul Robson (paul@robsons.org.uk)
+;		Date:		11th January 2017.
+;
+; ***************************************************************************************************************
+; ***************************************************************************************************************
+
+; ***************************************************************************************************************
+;
+;	Expression Evaluator : L -> R evaluation with parenthesis.
+;
+;	rSrc 			points to ASCIIZ expression, next character on exit. 
+;	rExprPC 		runs in this R(P), this is mandatory.
+;	rVarPtr.1 		points to variables
+; 	rParam1 		returned value of expression.
+;	rSpecialHandler	Routine (ends with sep rExprPC) which processes the provided character for special values
+;					e.g. side effect variables like ?. Char in D, zeroes D if the character is found and processed,
+;					in which case the result should be in rParam2
+;
+;	This does not report errors. VTL-2 does not. So if you have an error then the result returned may be somewhat
+;	unexpected and should not be relied on, despite what the VTL-2 manual hints :)
+;
+;	Note this routine breaks most of the registers R6-R15. If this is called recursively it must still run in
+;	rExprPC and the original rExprPC,rSrc,rParenthesisLevel and rSaveStack must be saved. rVarPtr does not change
+;	but must be restored if changed by an external routine.
+;
+;	This is a long single function of 170 bytes or thereabouts. The add/sub/lookup routines and lookup table towards
+;	the end can be moved to other pages if required ; it doesn't matter where these routines are in memory.
+;
+; ***************************************************************************************************************
 
 __EXPRExitDec:
-	dec 	rSrc 
+	dec 	rSrc 														; unpick bad source gets
 __EXPRExit:
-	inc 	r2
-	lda 	r2
+	glo 	rSaveStack 													; because we don't handle errors properly we 
+	plo 	r2 															; may have incomplete operations on exit.
+	ghi 	rSaveStack 													; (VTL-2 does not report expression syntax errors)
+	phi 	r2
+	dec 	r2 															; load top most expression which is the answer
+	lda 	r2 															; if it actually worked.
 	plo 	rParam1
 	lda 	r2
 	phi 	rParam1
@@ -11,10 +49,14 @@ __EXPRExit:
 
 EXPREvaluate:
 	sex 	r2 															; using X = 2 again
-	ldi 	-1 															; clear parenthesis level to -1 (first increment)
+	ldi 	0 															; clear parenthesis level to 0.
 	plo 	rParenthesisLevel 
-	ldi 	UtilityPage/256  											; set Utility Page.1
-	phi 	rUtilPC 
+
+	glo	 	r2 															; save original stack position
+	plo 	rSaveStack
+	ghi 	r2
+	phi 	rSaveStack 
+
 __EXPRNewLevel:	
 	ldi 	0 															; push $0000  + on the stack.
 	stxd 																; MSB first
@@ -30,6 +72,9 @@ __EXPRNewTerm:
 	plo 	rParam1
 	ghi 	rSrc
 	phi 	rParam1
+
+	ldi 	UtilityPage/256  											; set Utility Page.1
+	phi 	rUtilPC 
 	ldi 	ASCIIToInteger & 255 										; call the atoi() routine.
 	plo 	rUtilPC
 	mark
@@ -52,7 +97,7 @@ __EXPRNewTerm:
 	xri 	':'!'('														; is it new array ?
 	bz 		__EXPRArray
 	xri 	':' 														; so make it back to the correct character.
-	sep 	rExprPC 													; check for 'special ones'
+	sep 	rSpecialHandler 											; check for 'special ones'
 	bz 		__EXPRGotTerm 												; if found one, we've got a term.
 	shl 																; byte size to word size
 	plo 	rVarPtr 													; now point to variable
@@ -86,8 +131,12 @@ __EXPRGotCharacter:
 ;
 __EXPRGotTerm:  														; new term is in rParam2.
 	inc 	r2 															; point stack to operator.
-	ldr 	rParam1,__OperatorTable-1 									; rParam1 is the operator look up table.
+	ldi 	(__OperatorTable-2)/256 									; rParam1 is the operator look up table.
+	phi 	rParam1
+	ldi 	(__OperatorTable-2)&255
+	plo 	rParam1
 __EXPRFindOperation:
+	inc 	rParam1
 	inc 	rParam1
 	lda 	rParam1 													; look to see what it is.
 	bz 		__EXPRFoundOperation 										; end of table.
@@ -96,15 +145,19 @@ __EXPRFindOperation:
 __EXPRFoundOperation:	
 	lda 	r2 															; load the stacked value into rParenthesisLevel.1
 	phi 	rParenthesisLevel
-	ldn 	rParam1 													; put address of routine into rUtilPC
+	lda 	rParam1 													; put address of routine into rUtilPC
 	plo 	rUtilPC
-	lda 	r2 			
+	ldn 	rParam1
+	phi 	rUtilPC
+
+	lda 	r2 															; read TOS for into param1
 	plo 	rParam1
 	ldn 	r2
 	phi 	rParam1
 	mark  																; and call the routine.
 	sep 	rUtilPC
 	dec 	r2
+
 ;
 ;	Having got the result, check to see if it was divide, if so write out the remainder to '%'
 ;
@@ -124,7 +177,9 @@ __EXPRNotDivide:
 	stxd
 	glo 	rParam1
 	stxd
-
+;
+;	Get the next operation, this is normally stacked, except if ) in which case the bracketed operation is closed
+;
 __EXPRSkipSpace:
 	lda 	rSrc 														; get the next operator.
 	bz 		__EXPRExitDec 												; done the next operator.
@@ -135,12 +190,112 @@ __EXPRSkipSpace:
 	xri 	')' 														; get it back
 	stxd 																; push it on the stack.
 	br 		__EXPRNewTerm 												; and get the next term.
-
-
+;
+; 	Close the bracketed operation
+;
 __EXPRDestackBracket:
-	; pop rParam2 off the stack.
-	; adjust and test the parenthesis level counter. ) comment
-	; go back to the got-term
-	; maybe have 2 byte addresses for operator functions.
-	br 		__EXPRDestackBracket	
+	dec 	rParenthesisLevel 											; dec brackets
+	glo 	rParenthesisLevel 											; if zero it is end of expression ) so exit.	
+	bz 		__EXPRExit
 
+	inc 	r2 															; pop rParam2 off the stack.
+	lda 	r2
+	plo 	rParam2
+	ldn 	r2
+	phi 	rParam2
+
+	br 		__EXPRGotTerm 												; go back and do the stacked operation below
+
+; ***************************************************************************************************************
+;
+;	Look up table for binary operators. 0 signifies the end of the table and default, so it covers - > < =
+;
+; ***************************************************************************************************************
+
+oper macro chdb,addr
+	db 		chdb,addr & 255,addr / 256
+	endm 
+
+__OperatorTable:
+	oper 		'+',__OpAdd 
+	oper		'*',Multiply
+	oper 		'/',Divide
+	oper 		'@',__OpLookUp 
+	oper 		0,__OpSub
+
+; ***************************************************************************************************************
+;
+;								Addition. rParam1 := rParam1 + rParam2
+;
+; ***************************************************************************************************************
+
+__OpAdd:
+	sex		r2 														; rParam1 := rParam1 + rParam2
+	glo 	rParam2
+	str 	r2
+	glo 	rParam1
+	add	
+	plo 	rParam1
+	ghi 	rParam2
+	str 	r2
+	ghi 	rParam1
+	adc
+	phi 	rParam1
+	inc 	r2
+	return
+
+; ***************************************************************************************************************
+;
+;				Array Lookup. rParam1 := Memory['*' + rParam * 2]. '*' is the top of memory variable.
+;
+; ***************************************************************************************************************
+
+__OpLookUp: 														; rParam1 := Memory[* + rParam2 * 2]
+	sex 	r2
+	glo 	rParam2 												; double rParam2
+	shl
+	plo 	rParam2
+	ghi 	rParam2
+	rshl
+	phi 	rParam2
+	ldi 	'*' * 2 												; point VarPtr to '*' variable
+	plo 	rVarPtr
+	sex 	rVarPtr
+
+	glo 	rParam2 												; add * to rParam2 
+	add
+	plo 	rParam2		
+	inc 	rVarPtr
+	ghi 	rParam2
+	adc
+	phi 	rParam2 
+
+	lda 	rParam2 												; read rParam2 into rParam1
+	plo 	rParam1
+	ldn 	rParam2
+	phi 	rParam1
+	sex 	r2
+	inc 	r2
+	return
+
+; ***************************************************************************************************************
+;
+;								Subtraction. rParam1 := rParam1 - rParam2
+;									 (Also > < = which return 0 or 1)
+;
+; ***************************************************************************************************************
+
+__OpSub:															; rParam1 := rParam1 - rParam2
+	sex		r2
+	glo 	rParam2
+	str 	r2
+	glo 	rParam1
+	sm
+	plo 	rParam1
+	ghi 	rParam2
+	str 	r2
+	ghi 	rParam1
+	smb
+	phi 	rParam1
+	inc 	r2
+	return
--- vtl2/test.asm
+++ vtl2/test.asm
@@ -9,15 +9,18 @@ r0 = 0 															; not used (may be used in interrupt display)
 r1 = 1 															; interrupt register
 r2 = 2 															; stack pointer
 
-rVarPtr = 9 													; always points to variables.
-rExprPC = 10 													; used as P register in expression (mandated)
-rSrc = 11 														; source code.
+rVarPtr = 6 													; always points to variables.
+rExprPC = 7 													; used as P register in expression (mandated)
+rSrc = 8 														; source code.
+rSpecialHandler = 9 											; special variables handler.
+rParenthesisLevel = 10 											; bracket level (low byte)
+rSaveStack = 11 												; original value of stack pointer
+
 rUtilPC = 12 													; used as P register calling routines (not mandated)
 rSubPC = 13														; used as P register to call routines within routines
 rParam1 = 14 													; subroutine parameters/return values.
 rParam2 = 15
 
-rParenthesisLevel = 9 											; bracket level (low byte)
 
 ldr macro 	r,n
 	ldi 	(n)/256
@@ -29,18 +32,22 @@ ldr macro 	r,n
 
 	dis
 	db 		0
-	ldr 	r2,3FFFh
+	ldr 	r2,3FFFh 											; stack
 	sex 	r2
-	ldr 	rVarPtr,2800h
-	ldr 	rSrc,eString
-	ldr 	rExprPC,EXPRevaluate
+	ldr 	rVarPtr,2800h 										; varptr high byte only reqd
+	ldr 	rSrc,eString 										; evaluate the string
+	ldr 	rExprPC,EXPRevaluate 								; the code
+	ldr 	rSpecialHandler,SpecialHandler 						; dummy special handler.
 	mark
 	sep 	rExprPC
 	dec 	r2
 wait:
 	br 	wait
 
-buffer:
+SpecialHandler:
+	sep 	rExprPC
+
+
 	org 		100h
 	include 	expression.asm
 
@@ -51,75 +58,9 @@ UtilityPage:
 	include 	utility/divide.asm
 	include 	utility/atoi.asm
 
-
-__OpAdd:
-	sex		r2 														; rParam1 := rParam1 + rParam2
-	glo 	rParam2
-	str 	r2
-	glo 	rParam1
-	add	
-	plo 	rParam1
-	ghi 	rParam2
-	str 	r2
-	ghi 	rParam1
-	adc
-	phi 	rParam1
-	inc 	r2
-	return
-
-__OpSub:															; rParam1 := rParam1 - rParam2
-	sex		r2
-	glo 	rParam2
-	str 	r2
-	glo 	rParam1
-	sm
-	plo 	rParam1
-	ghi 	rParam2
-	str 	r2
-	ghi 	rParam1
-	smb
-	phi 	rParam1
-	inc 	r2
-	return
-
-__OpLookUp: 														; rParam1 := Memory[* + rParam2 * 2]
-	sex 	r2
-	glo 	rParam2 												; double rParam2
-	shl
-	plo 	rParam2
-	ghi 	rParam2
-	rshl
-	phi 	rParam2
-	ldi 	'*' * 2 												; point VarPtr to '*' variable
-	plo 	rVarPtr
-	sex 	rVarPtr
-
-	glo 	rParam2 												; add * to rParam2 
-	add
-	plo 	rParam2		
-	inc 	rVarPtr
-	ghi 	rParam2
-	adc
-	phi 	rParam2 
-
-	lda 	rParam2 												; read rParam2 into rParam1
-	plo 	rParam1
-	ldn 	rParam2
-	phi 	rParam1
-	sex 	r2
-	inc 	r2
-	return
-
-oper macro chdb,addr
-	db 		chdb,addr & 255
-	endm 
-
-__OperatorTable:
-	oper 		'+',__OpAdd 
-	oper		'*',Multiply
-	oper 		'/',Divide
-	oper 		'@',__OpLookUp 
-	oper 		0,__OpSub
-
 eString:
-	db 		"1+(2*3)",0
+;	db 		" \"A\"+1",0
+;	db 		":2)-1",0
+;	db 		"42 ) this is a comment",0
+;	db 		"4+255+",0
+	db 		"(4*5)-(2*3)*2",0
